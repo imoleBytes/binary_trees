c1dcca52a0ac19d6dd37d8e4f94c70c32c2463d5
imole <dremkay71@gmail.com>
2024-01-02 18:51:46 +0100

    task8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,61 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_postorder - function that goes through a binary tree
+*						using post-order traversal
+* @tree: pointer to root
+* @func: pointer to a function
+*/
+
+
+
+
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	// visit left
+	// visit right
+	// visit root
+
+	
+	binary_tree_postorder(tree->left, func);	
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
+
+
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+// void print_num(int n)
+// {
+//     printf("%d\n", n);
+// }
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+// int main(void)
+// {
+//     binary_tree_t *root;
+
+//     root = binary_tree_node(NULL, 98);
+//     root->left = binary_tree_node(root, 12);
+//     root->right = binary_tree_node(root, 402);
+//     root->left->left = binary_tree_node(root->left, 6);
+//     root->left->right = binary_tree_node(root->left, 56);
+//     root->right->left = binary_tree_node(root->right, 256);
+//     root->right->right = binary_tree_node(root->right, 512);
+
+//     binary_tree_print(root);
+//     binary_tree_postorder(root, &print_num);
+//     return (0);
+// }
+
