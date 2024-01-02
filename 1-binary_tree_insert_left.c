a54a78122b67d2382189357568659ac84b1811d1
imole <dremkay71@gmail.com>
2024-01-02 16:19:34 +0100

    task3

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 
+CHECK THIS FILE ITS NOT CORRECT
+
+
 /**
 * binary_tree_insert_left - creates a node and add to the left child
 *								of the parent.
@@ -43,17 +46,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-    binary_tree_t *root;
+// int main(void)
+// {
+//     binary_tree_t *root;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_print(root);
-    printf("\n");
-    binary_tree_insert_left(root->right, 128);
-    binary_tree_insert_left(root, 54);
-    binary_tree_print(root);
-    return (0);
-}
+//     root = binary_tree_node(NULL, 98);
+//     root->left = binary_tree_node(root, 12);
+//     root->right = binary_tree_node(root, 402);
+//     binary_tree_print(root);
+//     printf("\n");
+//     binary_tree_insert_left(root->right, 128);
+//     binary_tree_insert_left(root, 54);
+//     binary_tree_print(root);
+//     return (0);
+// }
